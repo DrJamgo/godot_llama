5bb8fce4bc1241e8b1e472c5d04450cef51f3ff5
Dr.Jamgo <drjamgo@hotmail.com>
2026-05-09 17:31:17 +0200

    FIX: convert to utf8

--- src/llama_context.cpp
+++ src/llama_context.cpp
@@ -351,7 +351,9 @@ String LlamaContext::_generate_internal(int p_max_tokens, const Dictionary &p_pa
 	if (p_params.has("grammar")){
 		const char* grammar_root = "root";
 		if (p_params.has("grammar_root"))
-			grammar_root = p_params["grammar_root"];
+			grammar_root = static_cast<String>(p_params["grammar_root"]).utf8().get_data();
+
+		const char* grammar = static_cast<String>(p_params["grammar"]).utf8().get_data();
 
 		llama_sampler * grammar_sampler = llama_sampler_init_grammar(vocab, p_params["grammar"], grammar_root);
 		if (grammar_sampler)
